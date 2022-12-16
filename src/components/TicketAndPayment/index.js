409985cc7bb38b5a787402976f35537ca55438f8
Jonatas <jonatasalmeidasm@gmail.com>
2022-12-16 12:49:11 -0300

    fix: added enrollment validation to access payment page

--- src/components/TicketAndPayment/index.js
+++ src/components/TicketAndPayment/index.js
@@ -4,9 +4,11 @@ import { Typography } from '@material-ui/core';
 import { Wrapper } from './Wrapper';
 import { TypeOnline } from './TypeOnline';
 import { TypePresencial } from './TypePresencial';
+import useEnrollment from '../../hooks/api/useEnrollment';
 
 export default function TicketAndPayment() {
   const [selectedType, setSelectedType] = useState('');
+  const { enrollment } = useEnrollment();
 
   function typeSelect(type) {
     setSelectedType(type);
@@ -16,15 +18,21 @@ export default function TicketAndPayment() {
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
       <Wrapper>
-        <StyledTypography variant="h5">Primeiro, escolha sua modalidade de ingresso</StyledTypography>
-        <TypePresencial onClick={() => typeSelect('Presencial')} type={selectedType}>
-          Presencial
-          <h6>R$ 250</h6>
-        </TypePresencial>
-        <TypeOnline onClick={() => typeSelect('Online')} type={selectedType}>
-          Online
-          <h6>R$ 100</h6>
-        </TypeOnline>
+        {enrollment ? (
+          <>
+            <StyledTypography variant="h5">Primeiro, escolha sua modalidade de ingresso</StyledTypography>
+            <TypePresencial onClick={() => typeSelect('Presencial')} type={selectedType}>
+              Presencial
+              <h6>R$ 250</h6>
+            </TypePresencial>
+            <TypeOnline onClick={() => typeSelect('Online')} type={selectedType}>
+              Online
+              <h6>R$ 100</h6>
+            </TypeOnline>
+          </>
+        ) : (
+          <>Por favor, finalize sua inscrição para ter acesso a esta página.</>
+        )}
       </Wrapper>
     </>
   );
