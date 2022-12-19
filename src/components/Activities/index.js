e59b4e19e8dbf91a582fa8171cbb23fe369e220e
Jonatas <jonatasalmeidasm@gmail.com>
2022-12-19 20:44:01 -0300

    fix: fixed a bug and made some code cleaning

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,23 +1,15 @@
-import { useEffect, useState } from 'react';
 import { StyledTypography } from '../TicketAndPayment';
 import { Wrapper } from './Wrapper';
 import useTicket from '../../hooks/api/useTicket';
 
 export default function ActivitiesSelection() {
   const { ticket } = useTicket();
-  const [paymentStatus, setPaymentStatus] = useState('');
-
-  useEffect(() => {
-    if (ticket) {
-      setPaymentStatus(ticket.status);
-    }
-  }, []);
 
   return (
     <>
       <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
-      <Wrapper paymentStatus={paymentStatus}>
-        {paymentStatus === 'PAID' ? (
+      <Wrapper paymentStatus={ticket?.status}>
+        {ticket?.status === 'PAID' ? (
           <>Atividades: Em breve!</>
         ) : (
           <span>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</span>
