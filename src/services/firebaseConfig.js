f0d3c1aa12996b76add406800365ad68b3634da4
Brunna Serafina <brunnaserafina@gmail.com>
2023-01-05 12:19:31 -0300

    fix: added environment variables

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDZNigdWjO0RkoEAGibgtI1iyggDdSD9tQ',
-  authDomain: 'drivent-f52a1.firebaseapp.com',
-  projectId: 'drivent-f52a1',
-  storageBucket: 'drivent-f52a1.appspot.com',
-  messagingSenderId: '285698501422',
-  appId: '1:285698501422:web:138637bad548fb77b1e8bf',
-  measurementId: 'G-PL2WMTL1JQ',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
